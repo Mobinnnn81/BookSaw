9643b253120ef61ceb0291b1a505619c06c12e3d
mobin <mobin@gmail.com>
2025-07-28 22:50:06 +0330

    server & database Configuration on project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using BookSaw;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<AppDbContext>();
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+{
+     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDatabase"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
